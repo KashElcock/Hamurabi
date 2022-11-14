43436596f8ac1ac91c8a816f388dc8d97d97527c
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-13 19:58:58 -0500

    Add files via upload

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -1,61 +1,85 @@
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hammurabi { //Hammurabi Class
 
     // Object Fields
-    private int priceOfBushels;
-    private int landValue;
     private int bushelsOwned;
-    private int population;
     private int acresOwned;
+    private int population;
+    private int totalDeaths;
+    private int percentDied;
+    private int year;
+    private int immigrants;
+    private int harvest;
+    private int starved;
+    private int priceOfLand;
+    private int priceOfBushels;
+    private static String userName;
+    static boolean plague = false;
 
 
     // ALL METHODS
 
         //Main method to run new game
+
     public static void main(String[] args) {
         new Hammurabi().playGame();
     }
 
+
         //Play Game Method
+
     void playGame() {
-        int year = 0;
-        int starved = 0;
-        int priceOfLand = 19;  // declare local variables here: grain, population, etc.// statements go after the declations
 
+        setUserName(getStringInput("Hello welcome to a new game of Hammurabi, please enter your name to continue"));
 
-
-
-        String userName = getStringInput("Hello welcome to a new game of Hammurabi, please enter your name to continue");
-
-        while (year < 10 && starved < 45  ) { //happy kingdom check && (newGame.getbushelsOwned() > (newGame.getPopulation()*20) )
-
+        while (year < 10 && starved < 45){
             year = year + 1;
-            int userInput = getIntegerInput("HELLO " + userName + "!\n" +
-                    "    You are in year "+ year +" of your ten year rule.\n" +
-                    "    In the previous year 0 people starved to death.\n" +
-                    "    In the previous year 5 people entered the kingdom.\n" +
-                    "    The population is now 100.\n" +
-                    "    We harvested 3000 bushels at 3 bushels per acre.\n" +
-                    "    Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
-                    "    The city owns 1000 acres of land.\n" +
-                    "At the current price of " + priceOfLand + " bushels per acre, would you like to buy or sell land this year?");
-            //buy or sell this year?
+
+            //NEW YEAR SUMMARY
+            int userInput = getIntegerInput("HELLO " + getUserName() + "!\n" +
+                    "    Welcome to year "+ getYear() +" of your ten year rule.\n" +
+                    "    Last year: "+ getStarved() + " people starved to death.\n" +
+                    "               "+ getImmigrants() +" people entered the kingdom.\n" +
+                    "    This year: The population is now "+ getPopulation() +".\n" +
+                    "               We harvested "+ getHarvest() + " bushels.\n" +
+                    "               Rats destroyed "+ 0 +" bushels, leaving " + getbushelsOwned() + " bushels in storage.\n" +
+                    "               The city owns " + getAcresOwned() + " acres of land.\n" +
+                    "At the current price of " + getPriceOfLand() + " bushels per acre, would you like to buy or sell land this year?" +
+                    "\nTo Buy: Enter 1\nTo Sell: Enter 2\nPress any other key to skip");
+
+            //BUY OR SELL LAND THIS YEAR?
             if(userInput == 1 ){
+                int buy = getIntegerInput("At the current price of " + priceOfLand + " bushels per acre, how many bushels worth of land would you like to buy?");
+                askHowManyAcresToBuy(priceOfLand,buy);
             } else if (userInput == 2) {
+                int sell = getIntegerInput("At the current price of " + priceOfLand + " bushels per acre, how many bushels worth of land would you like to sell?");
+                askHowManyAcresToSell(acresOwned);
+            } else {
+                System.out.println("Fine we'll do what we did last year");
             }
-            int buy = getIntegerInput("At the current price of " + priceOfLand + " bushels per acre, how many bushels worth of land would you like?");
-            askHowManyAcresToBuy(priceOfLand,buy);
 
+            //FEED POPULATION
+            int bushelsToFeedPopulation = getIntegerInput("Oh " + userName + ", don't forget to feed your people! \nHow many bushels of grain should we allocate?");
+            askHowMuchGrainToFeedPeople(bushelsToFeedPopulation);
 
-            priceOfLand = newCostOfLand();
-            //priceOfLand = (int) ((Math.random() * (50)) + 1 );
+            //PLANT GRAIN FOR NEXT YEAR
+            int howManyAcresToPlant = getIntegerInput("How many acres to plant with grain?, We must have enough acres, enough grain, and enough people to do the planting. Any grain left over will go into storage for next year");
+
+            //END OF YEAR OPERATIONS
+            setPriceOfLand(newCostOfLand());
+        }
+
+        if(getYear() == 10) {
+            System.out.println("Thank you for your great service to the Kingdom! The people are happy and your legacy secured! Enjoy retirement " + getUserName());
+        } else {
+            System.out.println("The people are furious and you've been overthrown!");
         }
     }
 
         // IO Methods
+
     public static void print(String output, Object... args) {
         System.out.printf(output, args);
     }
@@ -93,15 +117,9 @@ public class Hammurabi { //Hammurabi Class
         while (true);
     }
 
+
         //Setters and Getters
-    public void setPriceOfBushels(int priceOfBushels) {this.priceOfBushels = priceOfBushels;}
-    public int getPriceOfBushels() {return priceOfBushels;}
-    public int getLandValue() {
-        return landValue;
-    }
-    public void setLandValue(int landValue) {
-        this.landValue = landValue;
-    }
+
     public int getbushelsOwned() {
         return bushelsOwned;
     }
@@ -120,20 +138,77 @@ public class Hammurabi { //Hammurabi Class
     public void setAcresOwned(int acresOwned) {
         this.acresOwned = acresOwned;
     }
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+    public void setTotalDeaths(int totalDeaths) {
+        this.totalDeaths = totalDeaths;
+    }
+    public int getPercentDied() {
+        return percentDied;
+    }
+    public void setPercentDied(int percentDied) {
+        this.percentDied = percentDied;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getStarved() {
+        return starved;
+    }
+    public void setStarved(int starved) {
+        this.starved = starved;
+    }
+    public int getPriceOfLand() {
+        return priceOfLand;
+    }
+    public void setPriceOfLand(int priceOfLand) {
+        this.priceOfLand = priceOfLand;
+    }
+    public static String getUserName() {
+        return userName;
+    }
+    public static void setUserName(String userName) {
+        Hammurabi.userName = userName;
+    }
+    public static boolean isPlague() {
+        return plague;
+    }
+    public static void setPlague(boolean plague) {
+        Hammurabi.plague = plague;
+    }
+    public static void setRand(Random rand) {
+        Hammurabi.rand = rand;
+    }
+    public int getImmigrants() {
+        return immigrants;
+    }
+    public void setImmigrants(int immigrants) {
+        this.immigrants = immigrants;
+    }
+    public int getHarvest() {
+        return harvest;
+    }
+    public void setHarvest(int harvest) {
+        this.harvest = harvest;
+    }
 
         // GamePlay Methods
     static Random rand = new Random();
-    public Hammurabi(int landValue, int bushelsOwned, int population, int acresOwned) {
-        this.landValue = landValue;
+    public Hammurabi(int priceOfLand, int bushelsOwned, int population, int acresOwned) {
+        this.priceOfLand = priceOfLand;
         this.bushelsOwned = bushelsOwned;
         this.population = population;
         this.acresOwned = acresOwned;
     }
     public Hammurabi() {
-        this.landValue = 19;
         this.bushelsOwned = 2800;
         this.population = 100;
         this.acresOwned = 1000;
+        this.priceOfLand = 19;
     }
     public int askHowManyAcresToBuy(int price, int acresWantToBuy){
         int total=acresWantToBuy*price;  //Total Bushels needed to buy land
@@ -248,5 +323,6 @@ public class Hammurabi { //Hammurabi Class
 
         return grainseatenbyrats;
     }
+
 
 } //Hammurabi Class Ends
